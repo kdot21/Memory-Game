91c3b5ffaa5622438e05fd5f0806e0826b99f7f5
Kenneth Mancini <Kenneth@Kenneths-MacBook-Pro.local>
2016-09-17 18:19:22 -0400

    game logic

--- main.js
+++ main.js
@@ -1,16 +1,59 @@
-var cardOne = "queen"
-var cardTwo = "queen"
-var cardThree = "king"
-var cardFour = "king"
+console.log("JS file is connected HTML")
 
-if (cardOne === cardTwo) {
+ var cardOne = "queen"
+ var cardTwo = "queen"
+ var cardThree = "king"
+ var cardFour = "king"
+
+ if (cardOne === cardTwo) {
 	alert('You found a match!');	
 	} else {
 	alert('Sorry, try again.');
-}
+ }
 
-if (cardThree === cardFour) {
+ if (cardThree === cardFour) {
 	alert('You found a match!');	
 	} else {
 	alert('Sorry, try again.');
+ }
+
+var board = document.getElementById('game-board');
+
+var cards = ["queen", "queen", "king", "king",];
+
+var cardsInPlay = []
+
+function createBoard() {
+
+	for (var i = 0; i< cards.length; i++) {
+		var cardOne = document.createElement('div');
+		cardOne.className = 'card';
+		board.appendChild(cardOne);
+		return this;
+	}
+
+	for (var i = 0; i < cards.length; i++) {
+		var cardTwo = document.createElement('div');
+		cardTwo.className = 'card';
+		board.appendChild(cardTwo);
+		return this;
+	}
+
+	for (var i = 0; i < cards.length; i++) {
+		var cardThree = document.createElement('div');
+		cardThree.className = 'card';
+		board.appendChild(cardThree);
+		return this;
+	}
+
+	for (var i = 0; i < cards.length; i++) {
+		var cardFour = document.createElement('div');
+		cardFour.className = 'card';
+		board.appendChild(cardFour);
+		return this;
+	}
+
 }
+
+
+
